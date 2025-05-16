8ff8fbc8014775b106174b0ee3294e99e4fd4a70
rafaeldp7 <delapazr0721@gmail.com>
2025-05-16 21:27:07 +0800

    changed user._id on the auth.js login to user.id

--- routes/auth.js
+++ routes/auth.js
@@ -48,7 +48,7 @@ router.post("/login", async (req, res) => {
     const isMatch = await bcrypt.compare(password, user.password);
     if (!isMatch) return res.status(400).json({ msg: "Invalid credentials" });
 
-    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, { expiresIn: "1d" });
+    const token = jwt.sign({ id: user.id }, process.env.JWT_SECRET, { expiresIn: "1d" });
     res.json({ token, user });
   } catch (err) {
     console.error("Login Error:", err);
