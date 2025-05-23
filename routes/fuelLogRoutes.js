ed36fd6bfb60b4ff9aedf2a3a5a659d976537eea
rafaeldp7 <delapazr0721@gmail.com>
2025-05-23 08:26:13 +0800

    fixed bug in fuellogroutes

--- routes/fuelLogRoutes.js
+++ routes/fuelLogRoutes.js
@@ -3,6 +3,6 @@ const router = express.Router();
 const controller = require("../controllers/fuelLogController");
 
 router.get("/:userId", controller.getFuelLogsByUser);
-router.post("/", controller.addFuelLog);
+router.post("/", controller.createFuelLog);
 
 module.exports = router;
