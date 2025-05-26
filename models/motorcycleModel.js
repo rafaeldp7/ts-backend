19b1b245a5262ffad04c4d0d79206159252ef886
rafaeldp7 <delapazr0721@gmail.com>
2025-05-26 21:51:15 +0800

    refactored motorcycle model

--- models/motorcycleModel.js
+++ models/motorcycleModel.js
@@ -3,17 +3,17 @@ const mongoose = require("mongoose");
 const MotorcycleSchema = new mongoose.Schema(
   {
     model: { type: String, required: true, unique: true },
-    engineDisplacement: { type: Number, required: true }, // in cc
-    power: { type: String, required: true }, // e.g. '9.12hp @ 7,500rpm'
-    torque: { type: String, required: true }, // e.g. '9Nm @ 6,500rpm'
-    fuelTank: { type: Number, required: true }, // in Liters
-    fuelConsumption: { type: Number, required: true }, // in km/L
+    engineDisplacement: { type: Number }, // optional
+    power: { type: String },              // optional
+    torque: { type: String },             // optional
+    fuelTank: { type: Number },           // optional
+    fuelConsumption: { type: Number, required: true }, // required
     isDeleted: { type: Boolean, default: false }
-
   },
   {
     timestamps: true,
   }
 );
+
 
 module.exports = mongoose.model("Motorcycle", MotorcycleSchema);
