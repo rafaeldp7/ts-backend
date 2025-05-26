7a3d2cf27e0e7d12e2b7a057ca8646f84db09067
rafaeldp7 <delapazr0721@gmail.com>
2025-05-27 02:48:32 +0800

    wala pang mga middlewares sa gagsstationroutes

--- routes/gasStationsRoutes.js
+++ routes/gasStationsRoutes.js
@@ -7,12 +7,20 @@ const auth = require("../middleware/auth"); // For token validation
 // USER
 router.get("/nearby", gasStationController.getNearbyStations);
 
-// ADMIN
-router.get("/", auth, isAdmin, gasStationController.getAllStations);
-router.get("/analytics", auth, isAdmin, gasStationController.getAnalytics);
-router.get("/:id", auth, isAdmin, gasStationController.getStationById);
-router.post("/", auth, isAdmin, gasStationController.createStation);
-router.put("/:id", auth, isAdmin, gasStationController.adminUpdateStation);
-router.delete("/:id", auth, isAdmin, gasStationController.deleteStation);
+// ADMIN ito kapag naayos na yung middlewares
+// router.get("/", auth, isAdmin, gasStationController.getAllStations);
+// router.get("/analytics", auth, isAdmin, gasStationController.getAnalytics);
+// router.get("/:id", auth, isAdmin, gasStationController.getStationById);
+// router.post("/", auth, isAdmin, gasStationController.createStation);
+// router.put("/:id", auth, isAdmin, gasStationController.adminUpdateStation);
+// router.delete("/:id", auth, isAdmin, gasStationController.deleteStation);
+
+
+router.get("/",  gasStationController.getAllStations);
+router.get("/analytics",  gasStationController.getAnalytics);
+router.get("/:id",  gasStationController.getStationById);
+router.post("/",  gasStationController.createStation);
+router.put("/:id",  gasStationController.adminUpdateStation);
+router.delete("/:id",gasStationController.deleteStation);
 
 module.exports = router;
