e006ef876d9c23369a9d58beca15bd59dd70fa90
rafaeldp7 <delapazr0721@gmail.com>
2025-05-30 10:45:31 +0800

    commit

--- routes/analyticsRoutes.js
+++ routes/analyticsRoutes.js
@@ -10,10 +10,10 @@ router.get("/generate-daily", generateDailyAnalytics);
 router.get("/daily-history/:motorId", getMotorDailyAnalyticsHistory);
 router.get("/user-timeline/:userId", analyticsController.getUserAnalyticsTimeline);
 
-router.get("/motor-efficiency", analyticsController.getMotorEfficiencyStats);
+
 router.get("/fuel-log-trend/:userId", analyticsController.getUserFuelLogTrend);
 
-router.get("/efficiency-ranking", getMotorEfficiencyRanking);
+
 
 
 module.exports = router;
