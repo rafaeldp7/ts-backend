8845d234741f00d7dc547faab52746ff5ae96e35
rafaeldp7 <delapazr0721@gmail.com>
2025-05-30 10:26:37 +0800

    commit

--- routes/leaderboardAnalyticsRoutes.js
+++ routes/leaderboardAnalyticsRoutes.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const controller = require("../controllers/leaderboardAnalyticsController");
+const controller = require("../controllers/leaderboardsAnalyticsController");
 
 router.get("/monthly", controller.getMonthlyLeaderboard);
 
