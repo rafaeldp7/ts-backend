29666492129d896c95bf23048b3ef3c134b177f0
rafaeldp7 <delapazr0721@gmail.com>
2025-09-28 05:37:10 +0800

    fixed router updateFuelLevel

--- routes/userMotorRoutes.js
+++ routes/userMotorRoutes.js
@@ -28,7 +28,7 @@ router.get("/user/:id", getUserMotorsByUserId); // GET by userId
 router.post("/", createUserMotor); // POST new motor
 router.put("/:id", updateUserMotor); // PUT update motor
 router.delete("/:id", deleteUserMotor); // DELETE motor
-router.put("/:id/fuel", userMotorController.updateFuelLevel); // Update fuel level
+router.put("/:id/fuel", updateFuelLevel); // Update fuel level
 
 
 router.get("/user-overview/:userId", getUserOverviewAnalytics);
